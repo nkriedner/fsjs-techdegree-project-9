4b493626787dc78a8ae0515577d199e5b4f4436a
Nils Kriedner <nkriedner@googlemail.com>
2024-03-11 09:58:59 +0100

    Create all route entries for user and  courses

---
+++ routes.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const express = require("express");
+
+// Create a router instance
+const router = express.Router();
+
+// GET request to /api/users
+router.get("/users", (req, res) => {
+    res.status(200).json({ msg: "GET request to /api/users route" });
+});
+
+// POST request to /api/users
+router.post("/users", (req, res) => {
+    res.status(200).json({ msg: "POST request to /api/users route" });
+});
+
+// GET request to /api/courses
+router.get("/courses", (req, res) => {
+    res.status(200).json({ msg: "GET request to /api/courses route" });
+});
+
+// GET request to /api/courses/:id
+router.get("/courses/:id", (req, res) => {
+    res.status(200).json({ msg: "GET request to /api/courses/:id route" });
+});
+
+// POST request to /api/courses
+router.post("/courses", (req, res) => {
+    res.status(201).json({ msg: "POST request to /api/courses route" });
+});
+
+// PUT request to /api/courses/:id
+router.put("/courses/:id", (req, res) => {
+    // res.status(204);
+    res.json({ msg: "PUT request to /api/courses/:id route" });
+});
+
+// DELETE request to /api/courses/:id
+router.delete("/courses/:id", (req, res) => {
+    // res.status(204);
+    res.json({ msg: "DELETE request to /api/courses/:id route" });
+});
+
+module.exports = router;
